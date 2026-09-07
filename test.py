d4c9452b3943f8df86765f0e0ea22a57242f4880
Torsten Böhm <astroboehm@gmail.com>
2026-09-07 12:09:23 +0200

    gog

--- test.py
+++ test.py
@@ -3,12 +3,6 @@ import settings_sirius_espadons
 ls /Users/boehm/Desktop/Observations/Sirius_polarbase/Espadons/RAW/02feb10/
 N=nx.Night("/Users/boehm/Desktop/Observations/Sirius_polarbase/Espadons/RAW/02feb10/",**settings_sirius_espadons.get_kwargs())
 N.star4
-N.star4[0].plot_voie(1,[54,55,56])
-plt.show()
-import matplotlib.pyplot as plt
-plt.show()
-N.star4[0].plot_voie(1,[53,54,55,56])
-plt.show()
 N.star4[0].plot_voie(1,[52,53,54,55,56])
 plt.show()
 
