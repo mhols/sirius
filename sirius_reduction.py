5a37c9e981377189922c40644243def8b33e4a51
Matthias Holschneider <matthias.holschneider@gmail.com>
2026-09-07 13:05:43 +0200

    progressing

--- sirius_reduction.py
+++ sirius_reduction.py
@@ -4,9 +4,16 @@ import os
 import matplotlib.pyplot as plt
 
 DATADIR = os.environ['SIRIUS02FEB10'] 
-N=nx.Night(DATADIR,**settings_sirius_espadons.get_kwargs())
-N.star4
-#N.star4[0].plot_voie(1,[52,53,54,55,56])
-#plt.show()
-N.stokes
+
+
+def run1():
+    N=nx.Night(DATADIR,**settings_sirius_espadons.get_kwargs())
+    N.star4
+    #N.star4[0].plot_voie(1,[52,53,54,55,56])
+    #plt.show()
+    N.stokes
+
+
+if __name__ == '__main__':
+    run1()
 
